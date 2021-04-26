75ae8d4aaa224971eeda1c403003aa05fc244c83
Chanraksar Wat <chanraksarwat@gmail.com>
2021-04-26 16:20:55 -0500

    added apirouters and htmlrouters

---
+++ routes/htmlroutes.js
@@ -0,0 +1,18 @@
+// Depandencies
+const path = require('path');
+
+// Routing
+module.exports = function(app) {
+  // HTML GET Requests
+  app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
+
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+  
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+};
